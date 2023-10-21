0f18402ff9abc546a82ce31f0283c12d0e3ebd3d
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-10-22 02:24:53 +0600

    notification with value

---
+++ app/src/main/java/org/freedu/fcmnotification/MainActivity.kt
@@ -0,0 +1,23 @@
+package org.freedu.fcmnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.freedu.fcmnotification.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    var dataString:String=""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        if(intent!=null){
+            if(intent.hasExtra("key1")){
+                for(key in intent.extras!!.keySet()){
+                    dataString=dataString+intent.extras!!.getString(key)+"\n"
+                }
+                binding.textView.text = dataString
+            }
+        }
+    }
+}
